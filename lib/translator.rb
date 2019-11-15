b65692dc63f89e0be1cb5fc7dbf00230ce3f366f
Sean Tarzy <seantarzy@gmail.com>
2019-11-15 22:01:21 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -18,7 +18,7 @@ end
 
 def get_japanese_emoticon(file_path, emoticon)
   load_library(file_path)
-  if file_path.include?(emoticon)
+  if new_hash.include?(emoticon)
   return "get_emoticon"[emoticon]
 else 
   puts "Sorry, that emoticon was not found"
@@ -27,7 +27,7 @@ end
 
 def get_english_meaning(file_path, emoticon)
   load_library(file_path)
-  if file_path.include?(emoticon)
+  if new_hash.include?(emoticon)
   return "get_meaning"[emoticon]
 else 
   puts "Sorry, that emoticon was not found"
